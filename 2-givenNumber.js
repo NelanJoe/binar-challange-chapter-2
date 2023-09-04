3ad5e656adf615e4d76e4ba56abc0ff613fda082
NelanJoe <nelanjorelat@gmail.com>
2023-09-04 18:20:37 +0700

    fix: 2-givenNumber

--- 2-givenNumber.js
+++ 2-givenNumber.js
@@ -1,12 +1,13 @@
 const checkTypeNumber = (givenNumber) => {
-  // checking typeof params givenNumber is number or not
-  const result =
-    typeof givenNumber === "number" ? givenNumber : "Error: Invalid data type";
-
-  return result;
+  if (typeof givenNumber !== "number") {
+    console.log("Error: Invalid data type");
+  } else {
+    const result = givenNumber % 2 === 0 ? "GENAP" : "GANJIL";
+    console.log(result);
+  }
 };
 
-console.log(checkTypeNumber(42));
-console.log(checkTypeNumber(2));
-console.log(checkTypeNumber({}));
-console.log(checkTypeNumber([]));
+checkTypeNumber(42);
+checkTypeNumber(3);
+checkTypeNumber({});
+checkTypeNumber([]);
